037d4246fcbdf0888243a325913edd69f5553ed3
pocketful <iveta.jac@gmail.com>
2022-08-01 17:04:04 +0300

    fetch questions

--- client/src/pages/QuestionsPage.jsx
+++ client/src/pages/QuestionsPage.jsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { getFetch } from "../helpers/fetch";
+import { useAuthCtx } from "../store/authContext";
+
 function QuestionsPage() {
-  return <div>QuestionsPage</div>;
+  const [questionsArr, setQuestionsArr] = useState([]);
+
+  const history = useHistory();
+  const { token } = useAuthCtx();
+  if (!token) history.replace('/login');
+
+  async function getQuestions() {
+    try {
+      const data = await getFetch('/questions', token);
+      console.log('data:', data);
+      setQuestionsArr(data);
+    } catch (err) {
+      console.log('error in getQuestions: ', err);
+    }
+  }
+
+  useEffect(() => {
+    if (token) getQuestions();
+  }, []);
+
+  return (
+    <>
+      <h1>Questions</h1>
+    </>
+  );
 }
 
 export default QuestionsPage;
