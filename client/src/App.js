2b12f3c22514c3d1e9dda2cb1c89390d873e477a
pocketful <iveta.jac@gmail.com>
2022-08-01 15:02:15 +0300

    PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Switch } from 'react-router-dom';
 import NotFoundError from './components/Errors/NotFoundError';
 import Header from './components/Header/Header';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 import Container from './components/UI/Container/Container';
 import AddQuestionPage from './pages/AddQuestionPage';
 import AnswersPage from './pages/AnswersPage';
@@ -10,31 +11,31 @@ import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
-      <div className="App">
-        <Header />
-        <Container>
-          <Switch>
-            <Route path="/login">
-              <LoginPage />
-            </Route>
-            <Route path="/register">
-              <RegisterPage />
-            </Route>
-            <Route path="/add">
-              <AddQuestionPage />
-            </Route>
-            <Route path="/:id/answers">
-              <AnswersPage />
-            </Route>
-            <Route exact path="/">
-              <QuestionsPage />
-            </Route>
-            <Route path="*">
-              <NotFoundError />
-            </Route>
-          </Switch>
-        </Container>
-      </div>
+    <div className="App">
+      <Header />
+      <Container>
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/register">
+            <RegisterPage />
+          </Route>
+          <PrivateRoute path="/add">
+            <AddQuestionPage />
+          </PrivateRoute>
+          <Route path="/:id/answers">
+            <AnswersPage />
+          </Route>
+          <Route exact path="/">
+            <QuestionsPage />
+          </Route>
+          <Route path="*">
+            <NotFoundError />
+          </Route>
+        </Switch>
+      </Container>
+    </div>
   );
 }
 
