1d2be6ab5dbb1216c3bd4daca70d830c42d9ecde
pocketful <iveta.jac@gmail.com>
2022-08-01 15:06:02 +0300

    navlinks showing depends on the users login state

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,29 +3,42 @@ import style from './Header.module.css';
 import Flex from '../UI/Flex/Flex';
 import logo from '../../assets/img/logo.png';
 import Container from '../UI/Container/Container';
+import { useAuthCtx } from '../../store/authContext';
 
 function Header() {
+  const { isLoggedIn, logout } = useAuthCtx();
   return (
     <header className={style.header}>
       <Container>
-      <Flex jc="spaceBetween" dir="column">
-        {/* <div className={style.flex}> */}
+        <Flex jc="spaceBetween" dir="column">
+          {/* <div className={style.flex}> */}
           <Link to="/">
             <img className={style.img} src={logo} alt="logo" />
           </Link>
           <nav className={style.nav}>
-            <NavLink exact to="/" activeClassName={style.active}>
-              Questions
-            </NavLink>
-            <NavLink to="/add" activeClassName={style.active}>
-              Ask Question
-            </NavLink>
-            <NavLink to="/login" activeClassName={style.active}>
-              Login
-            </NavLink>
-            <NavLink to="/register" activeClassName={style.active}>
-              Register
-            </NavLink>
+            {isLoggedIn && (
+              <>
+                <NavLink exact to="/" activeClassName={style.active}>
+                  Questions
+                </NavLink>
+                <NavLink to="/add" activeClassName={style.active}>
+                  Ask Question
+                </NavLink>
+                <Link to="/login" onClick={logout}>
+                  Logout
+                </Link>
+              </>
+            )}
+            {!isLoggedIn && (
+              <>
+                <NavLink to="/login" activeClassName={style.active}>
+                  Login
+                </NavLink>
+                <NavLink to="/register" activeClassName={style.active}>
+                  Register
+                </NavLink>
+              </>
+            )}
           </nav>
         </Flex>
         {/* </div> */}
