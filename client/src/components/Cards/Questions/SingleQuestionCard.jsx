149fd4f02a12162e350b783e36b2eb5c494001ce
pocketful <iveta.jac@gmail.com>
2022-08-02 21:30:06 +0300

    delete a question

--- client/src/components/Cards/Questions/SingleQuestionCard.jsx
+++ client/src/components/Cards/Questions/SingleQuestionCard.jsx
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import formattedDate from '../../../helpers/date';
 import Image from '../../UI/Image/Image';
 import Icon from '../../UI/Icon/Icon';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { deleteFetch } from '../../../helpers/fetch';
+import toast from 'react-hot-toast';
+import { useAuthCtx } from '../../../store/authContext';
 
 // question_id, user_id, title, content, created_at, updated_at, archived, username, image
 function SingleQuestionCard(props) {
@@ -15,18 +18,41 @@ function SingleQuestionCard(props) {
     updated_at,
     username,
     image,
-    // onUpdate,
-    // onDelete,
   } = props.data;
-  // console.log('SingleQuestionCard', props);
+
+  const { token } = useAuthCtx();
+  const history = useHistory();
+  
   const createdAtFormatted = formattedDate(created_at);
   const updatedAtFormatted = formattedDate(updated_at);
+
+  async function deleteQueHandler(questionId) {
+    if (!token) {
+      toast.error('You have to login first.');
+      return;
+    }
+    window.alert('Are you sure you want to delete this question?');
+    try {
+      const deleteResult = await deleteFetch(`questions/${questionId}`, token);
+      // console.log('deleteResult:', deleteResult);
+      if (!deleteResult.success) {
+        // console.log('failed to delete');
+        toast.error(deleteResult.message);
+        return;
+      }
+      toast.success(deleteResult.message);
+      setTimeout(() => {
+        history.push('/');
+      }, 3000);
+    } catch (err) {
+      console.log('err in deleteQueHandler:', err);
+    }
+  }
 
   return (
     <article className={style.card}>
       <h3 className={style.title}>{title}</h3>
       <p className={style.content}>{content}</p>
-
       <div className={style.flex}>
         <div className={style.dates}>
           <p className={style.created}>
@@ -47,11 +73,7 @@ function SingleQuestionCard(props) {
         <span>
           Update{' '}
           <Link to={`/${question_id}/editQuestion`}>
-            <Icon
-              icon="fa-pencil"
-              size="small"
-              // onClick={() => onUpdate(question_id)}
-            />
+            <Icon icon="fa-pencil" size="small" />
           </Link>
         </span>
         <span>
@@ -59,7 +81,7 @@ function SingleQuestionCard(props) {
           <Icon
             icon="fa-trash"
             size="small"
-            // onClick={() => onDelete(question_id)}
+            onClick={() => deleteQueHandler(question_id)}
           />
         </span>
       </div>
