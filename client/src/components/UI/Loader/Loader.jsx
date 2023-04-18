c856c367311a9884baf6c1707689e2700364d634
pocketful <iveta.jac@gmail.com>
2023-04-18 19:33:47 +0300

    small Loader style version

--- client/src/components/UI/Loader/Loader.jsx
+++ client/src/components/UI/Loader/Loader.jsx
@@ -1,7 +1,16 @@
 import style from './Loader.module.css';
+import PropTypes from 'prop-types';
 
-function Loader() {
-  return <div className={style.loader}></div>;
+function Loader({ size = 'large' }) {
+  return (
+    <div className={style.loaderContainer}>
+      <div className={`${style.loader} ${style[size]}`}></div>
+    </div>
+  );
 }
+
+Loader.propTypes = {
+  size: PropTypes.oneOf(['small', 'large']),
+};
 
 export default Loader;
